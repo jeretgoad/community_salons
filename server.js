9392dd543e14181c1f393bbf6e5cf151bba70600
Bingxu Ren <bingxu.ren@centre.edu>
2017-08-10 10:51:35 -0500

    resolve port issue

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 8080;
 
 var mongoose = require('./config/mongoose');  //this line has to be in front of all other middleware functions
 var express = require('./config/express');
